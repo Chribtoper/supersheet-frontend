28c5fa260e201c327bc9937c41fa087a0f910a40
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-11-13 15:46:43 -0500

    test

---
+++ src/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const productSheetsEndpoint = 'http://localhost:3000/api/v1/product_sheets/';
+  const productsEndpoint = 'http://localhost:3000/api/v1/products/';
+  fetch(productsEndpoint)
+    .then(res => res.json())
+    .then(json =>
+      json.forEach(note => {
+      })
+    );
+
+}); // End DOMContentLoaded
