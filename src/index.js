4e847dd7e80ddc07d3a9be0c1b2bdd11a856cca3
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-11-13 19:45:10 -0500

    Added price to post

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   const productSheetsEndpoint = 'http://localhost:3000/api/v1/product_sheets/';
   const productsEndpoint = 'http://localhost:3000/api/v1/products/';
-  fetch(productsEndpoint)
-    .then(res => res.json())
-    .then(json =>
-      json.forEach(note => {
-      })
-    );
+  // fetch(productsEndpoint)
+  //   .then(res => res.json())
+  //   .then(json =>
+  //     json.forEach(note => {
+  //     })
+  //   );
+
+  
 
 }); // End DOMContentLoaded
